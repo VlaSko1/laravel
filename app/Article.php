7ae162573b3cdddb9b65270e96fcc3167f8d51d7
Vladimir <Vizion84@yandex.ru>
2020-05-25 22:30:43 +0300

    lesson_5 Last code from lesson 5

--- app/Article.php
+++ app/Article.php
@@ -6,6 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    public function allArticles()
+    {
+        return \DB::table('articles')->select(['id', 'title', 'text'])->get()->toArray();
+    }
+    public function getArticleById(int $id)
+    {
+        return \DB::table('articles')->find($id);
+            /*->select(['id', 'title', 'text', 'published'])
+            ->where('id', $id)
+            ->first();*/
+
+    }
+    public function getCount()
+    {
+        return \DB::table('articles')->count();
+    }
+
+
     public function getTitleAttribute($value)
     {
         return strtoupper($value);
--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -1,20 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Список статей</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('content')
 <div style="margin-left: 15px;">
-
-        {{$article}}
-
-
-
+    @if((bool)$article->published == true)
+        <h3>{{$article->title}}</h3>
+        <div>
+            {!! $article->text !!}
+        </div>
+    @else
+        <h3>Статья в разработке</h3>
+    @endif
 
 </div>
-</body>
-</html>
+@stop
